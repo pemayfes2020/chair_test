20c67deb9f6f64414b54606a3498b158a11805b2
yamakyo27 <yamakyo2718@gmail.com>
2020-03-22 16:52:24 +0900

    add cw ccw

--- pwmtest.py
+++ pwmtest.py
@@ -13,27 +13,42 @@ GPIO.setup(in1, GPIO.OUT)
 GPIO.setup(in2, GPIO.OUT)
 GPIO.setup(in3, GPIO.OUT)
 GPIO.setup(in4, GPIO.OUT)
-p = GPIO.PWM(in1, 50)  # 周波数50Hz
-p.start(0)
+pccw = GPIO.PWM(in1, 500)  # 周波数500Hz
+pcw = GPIO.PWM(in2, 500)  # 周波数500Hz
+pccw.start(0)
+pcw.start(0)
 #duty比
-duty = 20
+dutyccw = 90
+dutycw = 90
 
 try:
     while True:
-        # 正転
-        p.ChangeDutyCycle(duty)
+        """
+        # CCW
+        pccw.ChangeDutyCycle(dutyccw)
         #GPIO.output(in1, 1)
         GPIO.output(in2, 0)
         GPIO.output(in3, 0)
         GPIO.output(in4, 1)
         time.sleep(0.2)
+        """
+        # CW
+        GPIO.output(in1, 0)
+        pcw.ChangeDutyCycle(dutycw)
+        #GPIO.output(in2, 1)
+        GPIO.output(in3, 1)
+        GPIO.output(in4, 0)
+        time.sleep(0.2)
+        
+
 
 except KeyboardInterrupt:
     pass
 
 # ブレーキ
-p.stop()
-#GPIO.output(in1, 0)
+pccw.stop()
+pcw.stop()
+GPIO.output(in1, 0)
 GPIO.output(in2, 0)
 GPIO.output(in3, 1)
 GPIO.output(in4, 1)
